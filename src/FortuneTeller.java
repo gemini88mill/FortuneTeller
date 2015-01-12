cfc4c12f775ca6c855b4b60972e0186b96e2d183
Raphael <gemini88mill@gmail.com>
2015-01-11 22:31:53 -0500

    first commit

---
+++ src/FortuneTeller.java
@@ -0,0 +1,16 @@
+/**
+ * Created by raphael on 1/8/15.
+ *
+ * Fortune Teller (Horoscope) – A program that checks your horoscope on various
+ * astrology sites and puts them together for you each day.
+ */
+public class FortuneTeller {
+
+    
+
+    public static void main(String[] args){
+        //start program
+
+
+    }
+}
